03a287bd28194eeb34bb26687d9ba8e0e8b254c3
eonsang <2eonhworking@gmail.com>
2021-01-12 05:58:19 +0900

    set

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.use('/assets', express.static('assets'));
 
-app.use('*', (request, response) => {
-  console.log(path.join(__dirname, 'build/index.html'));
-  return response.sendFile(path.join(__dirname, 'build/index.html'));
+const root = path.join(__dirname, './build');
+app.use(express.static(root));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root });
 });
 
 const PORT = 8080;
