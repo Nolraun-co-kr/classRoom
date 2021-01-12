aecc38dfa0201cb016e19a61ad00e8ff69eb6f7b
eonsang <2eonhworking@gmail.com>
2021-01-12 16:22:54 +0900

    video custom

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -3,22 +3,31 @@ import VideoPlayer from './VideoPlayer';
 
 const videoJsOptions = {
   autoplay: false,
+  playsinline: false,
   playbackRates: [0.5, 1, 1.25, 1.5, 2],
   width: 720,
   height: 300,
   controls: true,
   fluid: true,
+  muted: false,
   sources: [
     {
       src: '//vjs.zencdn.net/v/oceans.mp4',
       type: 'video/mp4'
     }
-  ]
+  ],
+  controlBar: {
+    playToggle: true,
+    pictureInPictureToggle: true,
+    remainingTimeDisplay: true,
+    progressControl: true
+  }
+
 };
 
 const Index = () => {
   return (
-    <div>
+    <div className={'playerWrapper'}>
       <VideoPlayer {...videoJsOptions} />
     </div>
   );
