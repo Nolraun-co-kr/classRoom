1af609ccb4c18c5b7cab87edd0bbe4a1a8ee076b
eonsang <2eonhworking@gmail.com>
2021-01-13 19:56:59 +0900

    videojs 기능 작업중

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -4,10 +4,49 @@ import videojs from 'video.js';
 import 'video.js/dist/video-js.min.css';
 
 export default class VideoPlayer extends React.Component {
-  componentDidMount () {
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady () {
-      console.log('onPlayerReady', this);
+  constructor (props) {
+    super(props);
+    this.state = {
+      showSubTrack: false,
+      trackSource: this.props.trackSource
+    };
+
+    this.toggleSubTrack = this.toggleSubTrack.bind(this);
+  }
+
+  toggleSubTrack () {
+    this.setState({
+      showSubTrack: !this.state.showSubTrack
     });
+  }
+
+  componentDidMount () {
+    const _this = this;
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady () {
+      const hdButton = this.controlBar.addChild('button', {});
+      hdButton.addClass('vjs-button-icon-hd');
+
+      // change Theme
+      const themeButton = this.controlBar.addChild('button', {});
+      themeButton.addClass('vjs-button-icon-theme');
+
+      // sub Toggle track
+      const subTrackButton = this.controlBar.addChild('button', {});
+      subTrackButton.addClass('vjs-button-icon-subtitles');
+      subTrackButton.on('click', function (e) {
+        _this.props.onToggleShowBottomTrack();
+        const track = this.player_.textTracks_.tracks_[0];
+        if (_this.props.showBottomTrack) {
+          track.mode = 'showing';
+        } else {
+          track.mode = 'hidden';
+        }
+      });
+    });
+
+    // right toggle track
+    const rightTrackButton = this.player.controlBar.addChild('button', {});
+    rightTrackButton.addClass('vjs-button-icon-right-track');
   }
 
   componentWillUnmount () {
@@ -19,7 +58,11 @@ export default class VideoPlayer extends React.Component {
   render () {
     return (
       <div data-vjs-player>
-        <video ref={node => (this.videoNode = node)} className="video-js" />
+        <video ref={node => (this.videoNode = node)} className="video-js">
+
+          <track kind="captions" src={this.state.trackSource} srcLang="ko" label="Korean" default />
+
+        </video>
       </div>
     );
   }
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import VideoPlayer from './VideoPlayer';
 
-const videoJsOptions = {
-  autoplay: false,
-  playsinline: false,
-  playbackRates: [0.5, 1, 1.25, 1.5, 2],
-  width: 720,
-  height: 300,
-  controls: true,
-  fluid: true,
-  muted: false,
-  sources: [
-    {
-      src: '//vjs.zencdn.net/v/oceans.mp4',
-      type: 'video/mp4'
-    }
-  ],
-  controlBar: {
-    playToggle: true,
-    pictureInPictureToggle: true,
-    remainingTimeDisplay: true,
-    progressControl: true,
-    volumePanel: {
-      inline: false,
-      vertical: true
-    }
-  }
-
-};
-
 const Index = () => {
+  const [showBottomTrack, setShowBottomTrack] = useState(true);
+
+  const handleToggleShowBottomTrack = useCallback(() => {
+    setShowBottomTrack(!showBottomTrack);
+  }, [
+    showBottomTrack
+  ]);
+
+  const videoJsOptions = {
+    autoplay: false,
+    playsinline: false,
+    playbackRates: [0.5, 1, 1.25, 1.5, 2],
+    width: 720,
+    height: 300,
+    controls: true,
+    fluid: true,
+    muted: false,
+    sources: [
+      {
+        src: '//vjs.zencdn.net/v/oceans.mp4',
+        type: 'video/mp4'
+      }
+    ],
+    controlBar: {
+      playToggle: true,
+      pictureInPictureToggle: true,
+      remainingTimeDisplay: true,
+      progressControl: true,
+      ChaptersButton: true,
+      volumePanel: {
+        inline: false,
+        vertical: true
+      }
+    },
+    trackSource: '/assets/track/example.vtt',
+    showBottomTrack: showBottomTrack,
+    onToggleShowBottomTrack: handleToggleShowBottomTrack
+  };
+
   return (
     <div className={'playerWrapper'}>
       <VideoPlayer {...videoJsOptions} />
