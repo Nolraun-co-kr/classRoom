8765744da61118b6d6ff0dbf8c456b65c7a17306
eonsang <2eonhworking@gmail.com>
2021-01-14 20:55:26 +0900

    scss 정리

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,10 @@
 import Header from './Header';
-import Footer from './Footer';
 import MobileNav from './MobileNav';
 import MobileUserMenu from './MobileUserMenu';
 import Lnb from './Lnb';
 
 export {
   Header,
-  Footer,
   MobileNav,
   MobileUserMenu,
   Lnb
